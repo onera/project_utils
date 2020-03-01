e290bcebf03eb0e6418dac28e91006f49fc874d3
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-03-02 00:13:35 +0100

    Trying relative git-dir path: fail

--- scripts/git/submodule_utils.sh
+++ scripts/git/submodule_utils.sh
@@ -49,6 +49,77 @@ __git_sclone() {
 }
 export -f __git_sclone
 
+# With relative path for subsubmodules: not working
+#__git_config_submod_wd() {
+#  submod_path=$2
+#  echo "Configuring submodule $submod_path"
+#
+#  # init
+#  git submodule init
+#
+#  # path to come back to the main repo
+#  declare -i nb_sub_dir=`echo $submod_path | grep -o / | wc -l`
+#  to_main_repo=..
+#  for (( i=0; i<nb_sub_dir; ++i)); do
+#    to_main_repo+=/..
+#  done 
+#  echo $to_main_repo
+#
+#  # each subsub repo is also a sub repo, so
+#  # for each subsub repo, make the working dir be a pointer to the corresponding sub repo
+#  subsubmod_conf_paths=`git config --file .gitmodules --name-only --get-regexp path`
+#  for subsubmod_conf_path in $subsubmod_conf_paths; do
+#    if [[ $subsubmod_conf_path =~ submodule\.(.*)\.path ]]; then
+#      subsubmod_path=${BASH_REMATCH[1]}
+#      echo $subsubmod_path
+#      echo gitdir: $to_main_repo/.git/modules/$subsubmod_path > $subsubmod_path/.git
+#    fi
+#  done
+#}
+#export -f __git_config_submod_wd
+#
+#__git_sclone() { 
+#  # clone without submodules
+#  clone_res="$(LANG=en_US git clone $@ 2>&1)"
+#  echo $clone_res
+#  if (($? != 0)); then
+#    echo "Command \"git clone $@\" failed"; exit 1
+#  else
+#    echo "Successfully cloned the main repository"
+#  fi
+#
+#  # get the name of the folder cloned
+#  if [[ $clone_res =~ .*Cloning\ into\ \'(.*)\'.* ]]; then
+#    repository_name=${BASH_REMATCH[1]}
+#  else
+#    echo "In __git_sclone: unable to parse output of \"git clone $@\""; exit 1
+#  fi
+#
+#  # update submodules and create git "symlinks" from subsubmodules to main repository submodules
+#  (cd $repository_name
+#    if (($? != 0)); then
+#      echo "Unable to access folder \"$repository_name\""; exit 1
+#    fi
+#
+#    git submodule update --init
+#    if (($? != 0)); then
+#      echo "Cloning of submodules failed"; exit 1
+#    else 
+#      echo "Successfully cloned submodules"
+#    fi
+#
+#    git submodule foreach '__git_config_submod_wd $toplevel $sm_path'
+#    if (($? != 0)); then
+#      echo "Configuring submodules of submodules failed"; exit 1
+#    else 
+#      echo "Successfully configured submodules of submodules"
+#    fi
+#  )
+#
+#  ##git rev-parse HEAD | xargs git name-rev # name of the branch associated to HEAD
+#}
+#export -f __git_sclone
+
 __git_fetchall() {
   git fetch --recurse-submodules=yes "$@";
 }
