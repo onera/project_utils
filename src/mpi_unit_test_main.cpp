8dc06990b13d9de148eb5bda4c1741a63c1294bf
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-08-26 00:58:03 +0200

    mpi_unit_test_main: mpi may already be initialized!

--- src/mpi_unit_test_main.cpp
+++ src/mpi_unit_test_main.cpp
@@ -1,9 +1,24 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "std_e/unit_test/mpi/doctest.hpp"
 
+namespace {
+
+inline auto
+mpi_init_if_needed(int argc, char** argv) -> bool {
+  int flag;
+  MPI_Initialized(&flag);
+  bool already_init = bool(flag);
+
+  if (!already_init) {
+    MPI_Init(&argc, &argv);
+  }
+  return already_init;
+}
+
+}
 
 int main(int argc, char** argv) {
-  MPI_Init(&argc, &argv);
+  mpi_init_if_needed(argc,argv);
 
   doctest::Context ctx;
   ctx.setOption("reporters", "MpiConsoleReporter");
