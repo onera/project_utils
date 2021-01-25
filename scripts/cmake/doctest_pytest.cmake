7c7495ac30e37984cc635b78068920c4072db9d2
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-01-25 01:04:43 +0100

    Corrected test creation

--- scripts/cmake/doctest_pytest.cmake
+++ scripts/cmake/doctest_pytest.cmake
@@ -36,12 +36,13 @@ function(create_doctest)
     )
   endif()
 
-  set_tests_properties(${test_name} PROPERTIES LABELS "${label}")
-  set_tests_properties(${test_name} PROPERTIES SERIAL_RUN ${serial_run})
-  set_tests_properties(${test_name} PROPERTIES PROCESSORS ${n_proc})
-
-  # Fails in non-slurm
-  # set_tests_properties(${target_name} PROPERTIES PROCESSOR_AFFINITY true)
+  set_tests_properties(${test_name}
+    PROPERTIES
+      LABELS "${label}"
+      SERIAL_RUN ${serial_run}
+      PROCESSORS ${n_proc}
+      #PROCESSOR_AFFINITY true # Fails in non-slurm
+  )
 endfunction()
 
 
@@ -56,14 +57,25 @@ function(create_pytest)
   set(n_proc ${ARGS_N_PROC})
   set(test_name "${PROJECT_NAME}_pytest_${label}")
 
-  set(complete_test_folder "${PROJECT_SOURCE_DIR}/${tested_folder}")
+  # Test environment
+  set(ld_library_path ${PROJECT_BINARY_DIR}:$ENV{LD_LIBRARY_PATH})
+  set(pythonpath ${PROJECT_BINARY_DIR}:${PROJECT_SOURCE_DIR}:${CMAKE_BINARY_DIR}/external/pytest-mpi-check:$ENV{PYTHONPATH})
+
+  # Don't pollute the source with __pycache__
+  if (${Python_VERSION} VERSION_GREATER_EQUAL 3.8)
+    set(pycache_flag "-X pycache_prefix=${PROJECT_BINARY_DIR}")
+    set(pycache_env_var "PYTHONPYCACHEPREFIX=${PROJECT_BINARY_DIR}")
+  else()
+    set(pycache_flag "-B") # no way to specify __pycache__ location before python-3.8
+    set(pycache_env_var "PYTHONDONTWRITEBYTECODE=1")
+  endif()
 
   # -r : display a short test summary info, with a == all except passed (i.e. report failed, skipped, error)
   # -s : no capture (print statements output to stdout)
   # -v : verbose
-  # -Wignore : ignore warnings
-  # TODO if pytest>6, add --import-mode importlib (cleaner PYTHONPATH used by pytest)
-  set(pytest_command pytest ${complete_test_folder} -Wignore -ra -v -s --with-mpi)
+  # -Wignore : Python never warns (TODO why needed here?)
+  # TODO if pytest>=6, add --import-mode importlib (cleaner PYTHONPATH used by pytest)
+  set(pytest_command ${Python_EXECUTABLE} ${pycache_flag} -m pytest ${tested_folder} -Wignore -ra -v -s --with-mpi)
   if(${serial_run})
     add_test(
       NAME ${test_name}
@@ -79,24 +91,19 @@ function(create_pytest)
     )
   endif()
 
-  # > Set properties for the current test
-  set(pythonpath ${PROJECT_BINARY_DIR}:${PROJECT_SOURCE_DIR}:${CMAKE_BINARY_DIR}/external/pytest-mpi-check:$ENV{PYTHONPATH})
-  message("pythonpath = ${pythonpath}")
-  set_tests_properties(${test_name} PROPERTIES LABELS "${label}")
-  set_tests_properties(${test_name} PROPERTIES
-                       ENVIRONMENT PYTHONPATH=${pythonpath}
-                       DEPENDS t_${test_name})
-  #set_tests_properties(${test_name} PROPERTIES
-  #                     ENVIRONMENT PYTHONPATH=${PROJECT_BINARY_DIR}:${PROJECT_SOURCE_DIR}:${PROJECT_SOURCE_DIR}/${tested_folder}:${CMAKE_BINARY_DIR}/external/pytest-mpi-check:$ENV{PYTHONPATH}
-  #                     DEPENDS t_${test_name})
-  set_property(TEST ${test_name} APPEND PROPERTY
-                       ENVIRONMENT LD_LIBRARY_PATH=${PROJECT_BINARY_DIR}:$ENV{LD_LIBRARY_PATH})
-  set_property(TEST ${test_name} APPEND PROPERTY
-                       ENVIRONMENT PYTEST_PLUGINS=pytest_mpi_check)
-  set_tests_properties(${test_name} PROPERTIES SERIAL_RUN ${serial_run})
-  set_tests_properties(${test_name} PROPERTIES PROCESSORS ${n_proc})
-  # > Not working if not launch with srun ...
-  # set_tests_properties(${test_name} PROPERTIES PROCESSOR_AFFINITY true)
+  set_tests_properties(
+    ${test_name} 
+    PROPERTIES 
+      LABELS "${label}"
+      ENVIRONMENT "LD_LIBRARY_PATH=${ld_library_path};PYTHONPATH=${pythonpath}"
+      SERIAL_RUN ${serial_run}
+      PROCESSORS ${n_proc}
+      #PROCESSOR_AFFINITY true # Fails in non-slurm, Not working if not launch with srun
+  )
+
+  # TODO this one makes pytest execute nothing (WTF?)
+  #set_property(TEST ${test_name} APPEND PROPERTY
+  #                     ENVIRONMENT PYTEST_PLUGINS=pytest_mpi_check)
 endfunction()
 # --------------------------------------------------------------------------------
 
