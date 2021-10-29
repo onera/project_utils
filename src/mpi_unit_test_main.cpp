3db6a73e9054f24d1f19797d96b4453012aa0c1d
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-10-29 19:03:42 +0200

    mpi_unit_test_main: ask for MPI_THREAD_MULTIPLE if possible

--- src/mpi_unit_test_main.cpp
+++ src/mpi_unit_test_main.cpp
@@ -2,7 +2,15 @@
 #include "doctest/extensions/doctest_mpi.h"
 
 int main(int argc, char** argv) {
-  MPI_Init(&argc, &argv);
+  int provided_thread_support;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_thread_support);
+  if (provided_thread_support!=MPI_THREAD_MULTIPLE) {
+    std::string s;
+    if (provided_thread_support==MPI_THREAD_SINGLE) s = "MPI_THREAD_SINGLE";
+    if (provided_thread_support==MPI_THREAD_FUNNELED) s = "MPI_THREAD_FUNNELED";
+    if (provided_thread_support==MPI_THREAD_SERIALIZED) s = "MPI_THREAD_SERIALIZED";
+    std::cout << "WARNING: MPI_THREAD_MULTIPLE was asked, but only " + s + " was provided\n";
+  }
 
   doctest::Context ctx;
   ctx.setOption("reporters", "MpiConsoleReporter");
